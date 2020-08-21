ae489d23a81d470c542bb4662ff3de2bf9853b7b
Megan Freedman <megan@freedman.co.nz>
2020-08-21 14:31:35 -0400

    Add initial files

---
+++ src/test/java/tut4/AppTest.java
@@ -0,0 +1,22 @@
+package tut4;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
